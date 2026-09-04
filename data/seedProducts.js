ae41c15b851f65ec336cfc60ed3c58be617a0067
yuor7 <161158976+yuor7@users.noreply.github.com>
2026-09-04 14:47:29 +0530

    Update seedProducts.js

--- data/seedProducts.js
+++ data/seedProducts.js
@@ -28,7 +28,7 @@ const seedProducts = [
         mrp: 134900,
         price: 127400,
         image:
-          "https://images.unsplash.com/photo-1697284959152-32ef13855932?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+          "https://images.unsplash.com/photo-1759588071781-2c3ba9128497?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         emiPlans: buildEmiPlans(127400),
       },
       {
