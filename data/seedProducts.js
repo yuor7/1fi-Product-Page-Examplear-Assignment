2dc7defe03b18b5b5cf45efa73ffb4b49bc335c5
yuor7 <161158976+yuor7@users.noreply.github.com>
2026-09-04 14:45:16 +0530

    Change product image URL in seedProducts.js
    Updated product image URL for better quality.

--- data/seedProducts.js
+++ data/seedProducts.js
@@ -50,7 +50,7 @@ const seedProducts = [
         mrp: 154900,
         price: 147400,
         image:
-          "https://images.unsplash.com/photo-1607936854279-55e8a4c64888?q=80&w=1364&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+          "https://images.unsplash.com/photo-1697284959152-32ef13855932?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         emiPlans: buildEmiPlans(147400),
       },
     ],
